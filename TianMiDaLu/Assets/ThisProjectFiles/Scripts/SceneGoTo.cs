5cada88bd7b68f69504e0b1fa37b4d0d8d535610
molizhitian <228025204@qq.com>
2016-02-02 11:25:59 +0800

    刚刚开始
    就写了一点

---
+++ TianMiDaLu/Assets/ThisProjectFiles/Scripts/SceneGoTo.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneGoTo : MonoBehaviour {
+
+	
+    public void OnButtonStartGameClick(int levelIndex)
+    {
+        Application.LoadLevel(levelIndex);
+    }
+
+    public void OnButtonBattleModeClick(int levelIndex)
+    {
+        Application.LoadLevel(levelIndex);
+    }
+
+    public void OnButtonLuShiModeClick(int levelIndex)
+    {
+        Application.LoadLevel(levelIndex);
+    }
+}
